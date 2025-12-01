83a718654021fe3dbd61534ede21c662131ae087
Muazu Wakili <dealwithus14@gmail.com@gmail.com>
2025-12-01 18:54:56 +0100

    update

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 self.addEventListener("install", event => {
-  console.log("Service Worker installed");
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-  console.log("Service Worker activated");
+  event.waitUntil(clients.claim());
 });
 
 self.addEventListener("fetch", event => {
-  // Optional caching later
+  event.respondWith(fetch(event.request));
 });
